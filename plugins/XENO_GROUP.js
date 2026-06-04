8fe3d1defc097095e0d72d4a8910e49ea1a96166
xenocdn <xenocdn@gmail.com>
2026-06-04 18:42:18 +0530

    Update XENO_GROUP.js

--- plugins/XENO_GROUP.js
+++ plugins/XENO_GROUP.js
@@ -12,7 +12,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
     try {
         if (!isGroup) return reply('This command can only be used in a group.')
    
-        const user = m.mentioned[0] || m.quoted?.sender
+        const user = m.mentioned?.[0] || m.quoted?.sender
         if (!user) return reply('Please tag or reply to a user to promote.')
 
         await conn.groupParticipantsUpdate(from, [user], 'promote')
@@ -34,7 +34,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
     try {
         if (!isGroup) return reply('This command can only be used in a group.')
 
-        const user = m.mentioned[0] || m.quoted?.sender
+        const user = m.mentioned?.[0] || m.quoted?.sender
         if (!user) return reply('Please tag or reply to a user to demote.')
 
         await conn.groupParticipantsUpdate(from, [user], 'demote')
@@ -55,7 +55,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
     try {
         if (!isGroup) return reply('This command can only be used in a group.')
         
-        const user = m.mentioned[0] || m.quoted?.sender
+        const user = m.mentioned?.[0] || m.quoted?.sender
         if (!user) return reply('Please tag or reply to a user to remove.')
 
         await conn.groupParticipantsUpdate(from, [user], 'remove')
@@ -98,8 +98,6 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         if (!isGroup) return reply('This command can only be used in a group.')
-        if (!isBotAdmins) return reply('Bot must be an admin to use this command.')
-        if (!isAdmins) return reply('You must be an admin to use this command.')
 
         const goodbye = q
         if (!goodbye) return reply('Please provide a goodbye message.')
@@ -122,8 +120,6 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         if (!isGroup) return reply('This command can only be used in a group.')
-        if (!isBotAdmins) return reply('Bot must be an admin to use this command.')
-        if (!isAdmins) return reply('You must be an admin to use this command.')
 
         const welcome = q
         if (!welcome) return reply('Please provide a welcome message.')
