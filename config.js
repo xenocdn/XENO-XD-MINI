7b987d140ce7bd50d17b01ddea239fb25228d73b
xenocdn <xenocdn@gmail.com>
2026-06-04 18:11:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ module.exports = {
     AUTO_READ_STATUS: true,
     AUTO_REACT: false,
     AUTO_TYPING: false,
-    REACT_EMOJIS: ['❤','💕','😻','🧡','💛','💚','💙','💜','🎉','👋']
+    REACT_EMOJIS: ['❤','💕','😻','🧡','💛','💚','💙','💜','🎉','👋'],
+    MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://amalxerv3_db_user:<db_password>@cluster0.h7ueptd.mongodb.net/?appName=Cluster0"
 };
